5286b893e28164988cca563d2157784c45fbe8cb
PC <PC@DESKTOP-KF08QKF>
2024-05-25 13:23:53 +0700

    Long

---
+++ NewGame/src/ui/LevelCompletedOverlay.java
@@ -0,0 +1,13 @@
+package ui;
+
+import gamestates.Playing;
+
+public class LevelCompletedOverlay {
+	private Playing playing;
+	private UrmButton menu, next;
+
+	private public LevelCompletedOverlay(Playing playing) {
+		this.playing = playing;
+	}
+
+}
